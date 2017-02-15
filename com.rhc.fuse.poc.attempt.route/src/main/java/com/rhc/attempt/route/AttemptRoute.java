a825e2080ecae7a55dae91f614b2ad0f68f7a2d7
David Norris <Hillrunner2008@gmail.com>
2017-02-15 14:31:52 -0500

    removed transaction example

--- com.rhc.fuse.poc.attempt.route/src/main/java/com/rhc/attempt/route/AttemptRoute.java
+++ com.rhc.fuse.poc.attempt.route/src/main/java/com/rhc/attempt/route/AttemptRoute.java
@@ -1,11 +1,7 @@
 package com.rhc.attempt.route;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.spi.ComponentResolver;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,11 +21,11 @@ public class AttemptRoute extends RouteBuilder {
                 .log("${body}");
     }
 
-    @Reference(cardinality = ReferenceCardinality.MANDATORY,
-            policyOption = ReferencePolicyOption.GREEDY,
-            target = "(component=jms)")
-    public void setJmsComponent(ComponentResolver componentResolver) {
-        LOG.info("jms component resolved");
-    }
+//    @Reference(cardinality = ReferenceCardinality.MANDATORY,
+//            policyOption = ReferencePolicyOption.GREEDY,
+//            target = "(component=jms)")
+//    public void setJmsComponent(ComponentResolver componentResolver) {
+//        LOG.info("jms component resolved");
+//    }
 
 }
